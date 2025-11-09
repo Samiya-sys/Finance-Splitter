778b3bcbea83284d2c5c9a4404ab6fd5f43c0036
aymaan <aymaanpathan5@gmail.com>
2025-11-09 13:28:52 +0530

    Update project dependencies and moves  files
    - Upgraded Flask to version 3.1.2 and added mysql-connector-python version 8.1.0 in requirements.txt.
    - Add React Js For Frontend

--- config.py
+++
@@ -1,6 +0,0 @@
-MYSQL_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',             # your MySQL username
-    'password': 'admin@123', # your MySQL password
-    'database': 'finance_splitter_db'
-}
